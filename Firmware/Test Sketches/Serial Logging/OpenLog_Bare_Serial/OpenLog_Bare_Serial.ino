8ffeec28ddd7082e8ac9263c2daabf6d7567165f
Nathan Seidle <nathan.seidle@sparkfun.local>
2020-04-09 15:28:32 -0600

    Add extra baud rates and notes to serial testing sketches.

--- Firmware/Test Sketches/Serial Logging/OpenLog_Bare_Serial/OpenLog_Bare_Serial.ino
+++ Firmware/Test Sketches/Serial Logging/OpenLog_Bare_Serial/OpenLog_Bare_Serial.ino
@@ -1,8 +1,11 @@
 /*
   October 11th, 2019
-
-  Increase local serial, turn off except when syncing
-  Try better grade card
+  
+  A stripped down sketch to test the reception of different speeds of serial
+  and recording them to an SD card. Use with another board (RedBoard Artemis) 
+  and the ConstantSerialBlastTest.ino to send blocks of text. This sketch
+  will output the # of bytes received so you can quickly see if all 30,000
+  bytes made it over or if bytes were dropped.
 
   460800bps = 2.17 * 10^-6 or 2.17us per bit
   10 bits to the byte so 21.7us per byte
@@ -11,25 +14,29 @@
   16384 buffer size will allow for up to 355.532ms before bytes are dropped
   4096 buffer size will allow for up to 88.75ms before bytes are dropped
 
-  How big is RX FIFO within HAL? 32 bytes
-  Is there a 'nearly full' interrupt for the RX FIFO?
-  We could check the RX FIFO empty bit in the ISR in a loop
-  We could increase the ISR priority level
-
   ISR is currently 16us which supports 460800. We can burst mode to 8.5us.
+
+  With the most recent pull of the Apollo3 core from Github (ISR reduced, buffers
+  increased to 4096), and a 512GB card, this sketch can correctly receive/store
+  non-stop serial at 500000bps. Burst mode is not needed.
+
+  691200bps is possible in burst mode
   
 */
 
 //microSD Interface
 //-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 #include <SPI.h>
-#include <SdFat.h> //We do not use the built-in SD.h file because it calls Serial.print
+#include <SdFat.h>
 
 const byte PIN_MICROSD_CHIP_SELECT = 10;
 const byte PIN_MICROSD_POWER = 15; //x04
 
-SdFat sd;
-SdFile serialDataFile; //Record all incoming serial to this file
+SdFs sd; //exFat Support
+FsFile serialDataFile; //Record all incoming serial to this file
+
+//SdFat sd;
+//SdFile serialDataFile; //Record all incoming serial to this file
 //-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 
 long overallStartTime; //Used to calc the actual update rate.
@@ -100,6 +107,16 @@ void setup() {
 
   overallStartTime = millis();
   //enableBurstMode(); //Go to 96MHz
+
+  Serial.println("1)Set baud to 115200");
+  Serial.println("2)Set baud to 230400");
+  Serial.println("3)Set baud to 460800");
+  Serial.println("4)Set baud to 500000");
+  Serial.println("5)Set baud to 691200");
+  Serial.println("6)Set baud to 921600");
+  Serial.println("r)reset count");
+  Serial.println("b)Enable Burst Mode");
+  Serial.println("d)Disable Burst Mode");
 }
 
 void loop()
@@ -115,7 +132,6 @@ void loop()
         serialDataFile.write(incomingBuffer, sizeof(incomingBuffer)); //Record the buffer to the card
         incomingBufferSpot = 0;
       }
-
       charsReceived++;
     }
     lastSeriaLogSyncTime = millis();
@@ -162,18 +178,33 @@ void loop()
     }
     else if (incoming == '4')
     {
-      Serial.println("Serial set to: 921600");
-      SerialLog.begin(921600);
+      Serial.println("Serial set to: 500000");
+      SerialLog.begin(500000);
     }
     else if (incoming == '5')
     {
-      Serial.println("Serial set to: 500000");
-      SerialLog.begin(500000);
+      Serial.println("Serial set to: 691200");
+      SerialLog.begin(691200);
+    }
+    else if (incoming == '6')
+    {
+      Serial.println("Serial set to: 921600");
+      SerialLog.begin(921600);
     }
     else if (incoming == 'r')
     {
       Serial.println("Total count reset");
       charsReceived = 0;
+    }
+    else if (incoming == 'e')
+    {
+      Serial.println("Burst mode enabled");
+      enableBurstMode(); //Go to 96MHz
+    }
+    else if (incoming == 'd')
+    {
+      Serial.println("Burst mode disabled");
+      disableBurstMode(); //Go to 48MHz
     }
     else if (incoming == '\r' || incoming == '\n')
     {
